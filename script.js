a1f46b65230a40aae69d58af359b447585b683f1
sebahopp <sebahopp.kodehode@gmail.com>
2023-10-17 11:34:32 +0200

    added fuctionality to logo

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const logoButton = document.querySelector("#logo");
   const homeButton = document.querySelector(".Home #home");
-  const aboutLink = document.querySelector(".Nav #about-link"); // Use the section ID in the selector
-  const projectsLink = document.querySelector(".Nav #projects-link"); // Use the section ID in the selector
-  const contactLink = document.querySelector(".Nav #contact-link"); // Use the section ID in the selector
+  const aboutLink = document.querySelector(".Nav #about-link");
+  const projectsLink = document.querySelector(".Nav #projects-link");
+  const contactLink = document.querySelector(".Nav #contact-link");
+
+  logoButton.addEventListener("click", function () {
+    scrollToSection("landing");
+  });
 
   homeButton.addEventListener("click", function () {
     scrollToSection("landing");
